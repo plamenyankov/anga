76b87a6f1cd5fd107de0f8623a67ffd0a021e4bb
vagrant <vagrant@homestead>
2016-01-26 13:15:13 +0000

    env

--- public/themes/default/views/layouts/frontend.blade.php
+++ public/themes/default/views/layouts/frontend.blade.php
@@ -258,7 +258,7 @@
                                                 name="subscribe"
                                                 id="subscribe2"
                                                 type="checkbox"/>
-                                        Subscribe for news letters</label>
+                                        Subscribe for newsletters</label>
 
                                 </div>
                             </div>
